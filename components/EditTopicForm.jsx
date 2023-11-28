74f9e23cb594192b06e1db7b35e478ee3441f873
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-28 12:49:33 +0500

    Update EditTopicForm.jsx

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,4 +1,24 @@
 export default function EditTopicForm(){
 
-    return <div>Edit Toic</div>
+    return (<form onSubmit={handleSubmit}className="flex flex-col gap-3">
+    <input 
+        onChange={(e)=>setTitle(e.target.value)}
+    value={title}    
+    }
+        className="border border-state-500 px-8 py-2
+    type=text"
+    placeholder="Topic Title"/>
+    <input 
+        onChange={(e)=>setTitle(e.target.value)}
+        value={description}
+           
+        className="border border-state-500 px-8 py-2
+    type=text"
+    placeholder="Topic Description"/>
+    <button type="submit" className="bg-green-600 font-bold
+    text-white py-3 px-6 w-fit">
+    Add Topic
+    </button>
+    
+    </form>));
 }
