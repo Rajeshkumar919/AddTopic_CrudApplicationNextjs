b789065a0beb8d907ad60755d1b55980630489cf
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-28 12:58:34 +0500

    Update page.jsx

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,5 +1,25 @@
 import EditTopicFrom from "@/components/EditTopicForm";
-export default function editTopic(){
-return <EditTopicForm />;
+const getTopicById=async(id)=>{
+  try{
+    const res=await fetch(`http://localhost:3002/api/topics/${id}`,{
+      cache: "no-store",
+    });
+    if(!res.ok){
+      throw new Error("Failed to fetch topic");
+    }
+      return res.json();
+    catch(error){
+    console.log(error);
+    }
+  }
+}
+export default async function editTopic({params}){
+const {id}=params;
+  const {topic}=await getTopicById(id);
+  const {title,description}=topic;
+  console.log("id",id);
+  
+  
+  return <EditTopicForm id={id} title={title} description ={description}/>;
 
 }
